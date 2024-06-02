d40ec557b459c92170b57c597889dadea81767ac
mgbikash <mgbikash939@gmail.com>
2024-06-02 16:48:22 +0530

    create a test api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express'
+import { test } from '../controller/user.controller.js';
 
 const router=express.Router();
 
-router.get('/testtwo',(req,res)=>{
-    res.json({message:"hello wor"})
-})
-
+// router.get('/testtwo',(req,res)=>{
+//     res.json({message:"hello wor"})
+// })
+router.get('/testtwo',test)
 
 
 
